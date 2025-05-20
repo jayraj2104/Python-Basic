36166006e9baf7f41595c94e3085eaa402a16850
jayraj2104 <141935672+jayraj2104@users.noreply.github.com>
2025-05-20 12:55:50 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,5 @@
+#Program : FIBONACCI SERIES 
+a,b=0,1
+while(a < 10):
+    print(a)
+    c=a;a=b;b=c+b
---
+++ If.py
@@ -0,0 +1,8 @@
+# Program : If Condition 
+M = float(input("**ENTER YOUR MARKS ** \n "))
+if(M >= 90):print("**Excilent**")
+elif(M >= 75):print("**Good**")
+elif(M >= 50):print("**Average**")
+elif(M >= 40):print("**Poor**")
+else:print("**Good**")
+
